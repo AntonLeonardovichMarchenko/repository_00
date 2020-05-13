dfacc7b50e217556b2ce3d5b86076a1a8c147e83
Anton Marchenko <antonmarchenko@mail.ru>
2020-05-13 18:48:20 +0300

    вот сам скрипт написался вот он

--- Garden.py
+++ Garden.py
@@ -1,2 +1,16 @@
 # this is os_info
 
+# сбор информации об операционной системе и версии python
+
+# TODO запустить ЭТОТ скрипт и закомитить результат его работы (в файле os_info.txt)
+
+import platform
+import sys
+
+
+info = f"OS info is \n {platform.uname()}\n\n python version is {sys.version} {platform.architecture()}"
+
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
